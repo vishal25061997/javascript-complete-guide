22c6a5b20e5b171e0bc24d2faa22d7e0cc18cf30
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-04 18:43:21 +0530

    Day1-2

---
+++ day 1-2.js
@@ -0,0 +1,128 @@
+// LEVEL-1
+
+// 1
+// let Name = '30 days Of Javascript'
+// console.log(Name.substring(0,2))
+
+// 2
+
+// let Sub = 'Maths'
+// console.log(Sub)
+
+// 3
+
+// let Course = 'JavaScript'
+// console.log(Course.length)
+
+// 4
+
+// let Name= 'Vishal Yadav'
+// console.log(Name.toUpperCase())
+
+// 5
+
+// let Name = 'vISHAL yADAV'
+// console.log(Name.toLowerCase())
+
+// 6
+
+// let sub ="javaScript"
+// console.log(sub.substring(4,10))
+
+// 7
+
+// let Random ="30 Days Of JavaScript"
+// console.log(Random.slice(3,21))
+
+// 8
+
+// let Word='JavaScript'
+// console.log(Word.includes('Script'))
+
+// 9
+
+// let Name = 'Vishal Yadav'
+// console.log(Name.split())
+
+// 10
+
+// let Course = "30 Days Of Javscript"
+// console.log(Course.split())
+
+// 11
+
+// let Company = "Google, Microsoft Apple, IBM , Oracle, Amazon"
+// console.log(Company.split(' '))
+
+// 12
+
+// let Course = '30 Days Of Javascript '
+// console.log(Course.replace('Javascript','Python'))
+
+// 13
+
+// let Course = ' 30 Days Of JavaScript'
+// console.log(Course.charAt(10))
+
+// 14
+
+// let Course = '30 days Of JavaScript'
+// console.log(Course.charCodeAt('J'))
+
+// 15
+
+// let Sub = '30 Days Of JavaScript'
+// console.log(Sub.indexOf('a'))
+
+// 16
+
+// let Sub = '30 Days Of JavaScript'
+// console.log(Sub.lastIndexOf('a'))
+
+// 17
+
+
+// let Sub = 'You cannot end a sentence with because because because is a conjunction'
+// console.log(Sub.indexOf('because'))
+
+// 18
+
+
+// let Sub = 'You cannot end a sentence with because because because is a conjunction'
+// console.log(Sub.lastIndexOf('because'))
+
+// 19
+
+// let Sub = 'You cannot end a sentence with because because because is a conjunction'
+// console.log(Sub.search('because'))
+
+// 20
+
+// let Course = '   30 Days Of Javascript   '
+// console.log(Course.trim())
+
+// 21
+
+// let Course = '30 Days Of Javascript '
+//  console.log(Course.startsWith('30'))
+
+// 22
+
+// let Course = '30 Days Of JavaScript'
+//  console.log(Course.endsWith('JavaScript'))
+
+// 23
+
+// let Course = '30 Days Of Javascript '
+//  console.log(Course.match('a'))
+
+// 24
+
+// let addAll = '30 Days Of'
+// console.log(addAll.concat(' ','Javascript',' ','30 Days Of Javascript'))
+
+// 25
+
+// let Course = '30 Days Of Javascript '
+//  console.log(Course.repeat(2))
+
