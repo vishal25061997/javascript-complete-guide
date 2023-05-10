ff6425d88174aa528e5c2f8d0e3916a37232acbb
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-10 15:26:07 +0530

    day 2

---
+++ Day 2-3.js
@@ -0,0 +1,75 @@
+// LEVEL 2
+
+// 1
+
+// let a = 'There is no exercise better for the heart than reaching down and lifting people up'
+// let b = 'help'
+// console.log(`The quote '${a}' by John Holmes teaches us to ${b} one another `)
+
+// 2
+
+// let MotherTeresa = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+// console.log(MotherTeresa)
+
+// 3
+
+// let a='10'
+// let b= 10
+// c= parseInt(a)
+// console.log(typeof(c))
+// console.log(a==c)
+
+// 4
+
+// let num1 = '9.81'
+// let num2 = 10
+// let numFloat = parseFloat(num1)
+// num3= Math.round(numFloat)
+// console.log(num3== num2)
+
+// 5
+// let a = 'python'
+// let b = 'jargon'
+// let c = a.search('on')
+// let d = b.search('on')
+// console.log(c==d)
+
+// 6
+
+// let a = 'I hope this course is not full of jargon'
+// let b = 'jargon'
+// let c = a.search('jargon')
+// console.log(b==c)
+
+
+// 7
+
+// const num = Math.random()*111
+// console.log(num)
+
+// 8
+
+// const num = Math.random()*(50)+50
+// console.log(num)
+
+// 9
+
+// const num = Math.random()*255
+// console.log(num)
+
+// 12
+
+// const str = 'You cannot end a sentence with because because because is a conjunction'
+// console.log(str.slice(31,55))
+
+// 13
+// let count="love"
+// let str = "love is the best thing in this world. Some found their love and some are still looking for their love"
+// for (let i=0 ; i<str.length;i++){
+//    if(str.charAt(i)=='love'){
+//     console.log(count)
+//     count++
+//    }
+   
+// }
+
