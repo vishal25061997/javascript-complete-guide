b08086b976646e3e577f70e2ec3dd5b7197e23f8
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-11 17:48:02 +0530

    day3

---
+++ day-3.js
@@ -0,0 +1,92 @@
+// 1-
+// let FirstName = 'Vishal'
+// console.log(typeof(FirstName))
+
+// let LastName = "Yadav"
+// console.log(typeof(LastName))
+
+// let country = "INDIA"
+// console.log(typeof(country))
+
+// let city ='Jhansi'
+// console.log(typeof(city))
+
+// let age = 24
+// console.log(typeof(age))
+
+// let isMarried = true
+// console.log(typeof(isMarried))
+
+// 2
+
+// let num1 = '10'
+// let num2 = 10
+// console.log(typeof(num1)==typeof(num2))
+
+// 3
+
+// let num1 = parseInt('9.8')
+// let num2 = 10
+// console.log(typeof(num1)==typeof(num2))
+
+// 4
+
+// let num = 9 > 4
+// console.log(num)
+
+// let num1 = true
+// console.log(num1)
+
+// let num2 = 'hello'
+// console.log(typeof(num2)=== 'string')
+
+// 5
+
+// let num = 2 > 5;
+// console.log(num)
+
+// let isMarried = 'false'
+// console.log(isMarried)
+
+// let num2 = !true
+// console.log(num2)
+
+
+// 6
+
+// let a = 4 > 3
+// let b = 4 >= 3
+// let c = 4 < 3
+// let d = 4 <= 3
+// let e = 4 == 4
+// let f = 4 === 4
+// let g = 4 != 4
+// let h = 4 !== 4
+// let i = 4 != '4'
+// let j = 4 == '4'
+// let k = 4 === '4'
+// console.log(a,b,c,d,e,f,g,h,i,j,k)
+
+// let num1 = 'python'
+// let num2 = 'jargon'
+
+// console.log(num1.length==num2.length)
+
+// 6
+
+// let a = 4 > 3 && 10 < 12
+// let b = 4 > 3 && 10 > 12
+// let c = 4 > 3 || 10 < 12
+// let d = 4 > 3 || 10 > 12
+// let e = !(4 > 3)
+// let f = !(4 < 3)
+// let g = !(false)
+// let h = !(4 > 3 && 10 < 12)
+// let i = !(4 > 3 && 10 > 12)
+// let j = !(4 === '4')
+
+// console.log(a,b,c,d,e,f,g,h,i,j)
+
+// let name1 = "dragon "
+// let name2 = "python"
+// console.log(name1.includes('on'),(name2.includes('on')));
