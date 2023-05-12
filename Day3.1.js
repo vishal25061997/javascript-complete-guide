ded2757e84b0a18737ce13aabed1fcb689c7fdc7
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-13 00:58:23 +0530

    Day3.1

---
+++ Day3.1.js
@@ -0,0 +1,75 @@
+// 7
+//   let now = new Date()
+//    console.log(now.getFullYear())
+//    console.log(now.getMonth())
+//    console.log(now.getDate())
+//    console.log(now.getDay())
+//    console.log(now.getHours())
+//    console.log(now.getMinutes())
+ 
+
+// LEVEL 2
+
+// 1
+
+// let base = prompt('enter base')
+// let height= prompt('enter height')
+//  let area = 1/2*(base)*(height)
+//  console.log(area)
+
+// 2
+
+// let a = prompt("enter side a");
+// let b = prompt("enter side b");
+// let c = prompt("enter side c");
+// let perimeter = (a+b+c);
+// console.log(perimeter)
+
+// 3
+
+// let length = parseFloat(prompt("enter length"))
+// let width =parseFloat( prompt("enter width"))
+// let area =(length*width)
+// let perimeter = (2*(length+width))
+// console.log(area , perimeter)
+
+// 4
+
+// let pi = 3.14;
+// let r = prompt("enter radius");
+// let area = (pi*r*r);
+// let perimeter = (2*pi*r)
+// console.log(area , perimeter)
+
+// 5
+
+// const equation = {
+//     slope: 2,
+//     xIntercept: -1,
+//     yIntercept: -2
+//   };
+  
+//   console.log(`Slope: ${equation.slope}`);
+//   console.log(`X-Intercept: (${equation.xIntercept}, 0)`);
+//   console.log(`Y-Intercept: (0, ${equation.yIntercept})`);
+
+// 6
+
+// const point1 = {
+//     x:2,
+//     y:2
+// }
+// const point2 ={
+//     x:6,
+//     y:10
+// }
+// const slope=(point2.y - point1.y)/(point2.x - point1.x);
+// console.log(` The slope between (${point1.x}, ${point1.y} and (${point2.x}, ${point2.y}) is ${slope})`)
+
+// 9
+
+// let hour = prompt("enter hours");
+// let Rhour = prompt ("enter rate per hour");
+// let pay = (hour*Rhour)
+// console.log(`Your weekly earning is ${pay}`)
+
