5fcb6015f98f0f725b13e6fc2ecdc54414b99a30
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-15 02:05:50 +0530

    last questions day 3

---
+++ Day 3.2.js
@@ -0,0 +1,41 @@
+// 10
+
+// let name = prompt('enter name');
+// if (name.length> 7){
+//   console.log('name is lomg long')
+// }
+// else{
+//   console.log('name is to short')
+// }
+
+// 11
+
+//let firstName = 'Asabeneh'
+// let lastName = 'Yetayeh'
+// if(firstName.length > lastName.length){
+//   console.log('Your first name, Asabeneh is longer than your family name, Yetayeh')
+// }
+
+// 12
+
+// let myAge = 250
+// let yourAge = 25
+// let ageDiff = myAge-yourAge
+// console.log(`I am ${ageDiff} years olderthan you`)
+
+// 13
+// let age = prompt('enter age')
+// if (age <= 18){
+//     console.log(`You are ${age} you are not allwed to drive`)
+// }else{
+//     console.log(`Your are ${age} you are old enough to drive`)
+// }
+
+// 14
+
+// let date = new Date();
+// let hours = date.getHours(); 
+// let minutes = date.getMinutes(); 
+// let seconds = date.getSeconds()
+// let time = `${hours}:${minutes}:${seconds}`;
+// console.log(time);
