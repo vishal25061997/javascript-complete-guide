3defc95db1b2372a836dfd528180ae87754abdcd
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-15 20:16:07 +0530

    If,else if,switch,ternary-operator

---
+++ Day4.js
@@ -0,0 +1,93 @@
+//Day 4
+//LEVEL 1
+
+// 1
+
+// let age = prompt("Enter your age:"); 
+
+// if (age >= 18) {
+//   console.log("You are old enough to drive!"); 
+// } else {
+//   let yearsToWait = 18 - age;
+//   console.log(`You left  ${yearsToWait}  years to drive.`); 
+// }
+
+// 2
+
+// let myAge = 25;
+// let age = prompt('enter age');
+// let ageGap= age-myAge
+// if (age> myAge){
+//     console.log(`You are ${ageGap} years older than me`)
+// }
+// else {
+//     console.log('you are not older than me')
+// }
+
+// 3
+
+// let a =4;
+// let b =3;
+// if(a>b){
+//     console.log('a is greater  than b')
+// }
+// else{
+//     console.log('a is smaller than b')
+// }
+
+//  a>b ? console.log('a is greater  than b'): console.log('a is smaller than b');
+
+// 4
+
+// let num = prompt('enter number')
+// if (num%2==0){
+//     console.log(`${num} is an even number`)
+
+// }
+// else {
+//     console.log(`${num} is an odd number`)
+// }
+
+// LEVEL 2
+
+// 1
+ 
+// let num = prompt('enter number')
+// if (90<=num<=100){
+//     console.log('A')
+// }else if(70<=num<=89){
+//     console.log('B')
+// }else if(60<=num<=69){
+// console.log('C')
+// }else if(50<=num<=59){
+//     console.log('D')
+// }else if(0<=num<=49){
+//     console.log('F')
+// }else{
+//     console.log('Wrong Input')
+// }
+
+// 2
+
+// let month = prompt('enter month name')
+
+// if (month=='september' || 'october'|| 'november'){
+//     console.log('season is Autum')
+// }else if(month=='december'|| 'january'|| 'february'){
+//     console.log('season is Winter')
+// }else {
+//     console.log('season is Summer')
+// }
+
+// 3
+
+// let day = prompt("Enter a day ");
+
+// let weekendDays = ["Saturday", "Sunday"]; 
+
+// if (weekendDays.includes(day)) {
+//   console.log(`${day} is a weekend .`); 
+// } else {
+//   console.log(`${day} is a working day.`); 
+// }
+
