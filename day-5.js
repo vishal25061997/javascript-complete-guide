7184a650c86f72de543e1861f9cbb5cf28e9ccc1
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-17 14:35:31 +0530

    day-5

---
+++ day-5.js
@@ -0,0 +1,88 @@
+// LEVEL 1
+
+// 1
+
+// let arr = []
+
+// 2
+
+// let arr = [2, 3, 'banana',5,8, 'apple']
+
+// 3
+
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB'
+// ]
+
+// console.log(webTechs.length)
+
+// 4
+
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB'
+// ]
+
+// console.log(webTechs[0],webTechs[3], webTechs[6])
+
+// 5
+
+// let arr = [2, 'banana', true, {vishal:'yadav',
+//  saket:'thakur'},6, 'apple']
+// console.log(arr.length)
+
+
+// 11
+
+// let companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' , 'Amazon']
+
+// console.log(companies)
+
+// 12
+
+// console.log(comapnies.toUppercase)
+
+// 13
+
+// const companies = ['facebook', 'google', 'amazon', 'zomato']
+// let index = companies.indexOf('zomato')  
+
+// if(index === -1){
+//    console.log('This comapany does not exist in the array')  
+// } else {
+//     console.log('This comapany does exist in the array')
+
+// 14
+
+// 15
+
+//  const companies = ['facebook', 'google', 'amazon', 'zomato','apple']
+// console.log(companies.sort())
+
+// 16
+
+// let arr = [1,4,2,6,5,9,]
+//   console.log(arr.reverse())
+
+// 17
+
+// const companies = ['facebook', 'google', 'amazon','zomato','apple']
+
+// console.log(companies.slice(3,6))
+
+// 18
+
+// let arr = [1,3,,5,2,6,7,9,8]
+
+// console.log(arr.splice())
