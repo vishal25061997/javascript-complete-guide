86d78614e86a83ce61e38c715a444dcc134610ca
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-26 00:35:15 +0530

    day 6

---
+++ day 6.js
@@ -0,0 +1,155 @@
+// 10
+// let sum=0;
+// for (let i=0; i<=100; i++){
+//   sum+= i
+// }
+// console.log(sum)
+
+// 11
+
+// let sum =0;
+// let gSum=0;
+// for (let i=0; i<=100; i++){
+//   if (i%2==0){
+//     sum+=i
+//   }
+//   else {
+//     gSum+=i
+// }}
+//   console.log(`The sum of all even no. ${sum}`)
+//   console.log(`The Sum of all odd no. ${gSum}`)
+
+
+// 12
+
+// let sum =0;
+// let gSum=0;
+// for (let i=0; i<=100; i++){
+//   if (i%2==0){
+//     sum+=i
+//   }
+//   else {
+//     gSum+=i
+// }}
+//   console.log(`The sum of all even no. ${sum}`)
+//   console.log(`The Sum of all odd no. ${gSum}`)
+
+// 13
+
+// let sum =0;
+// let gSum=0;
+// for (let i=0; i<=100; i++){
+//   if (i%2==0){
+//     sum+=i
+//   }
+//   else {
+//     gSum+=i
+// }}
+// let arr= `${sum},${gSum}`
+// let newArray=arr.split()
+// console.log(newArray)
+
+// 14
+
+// function generateRandomArray() {
+//   var randomArray = [];
+//   for (var i = 0; i < 5; i++) {
+//     var randomNumber = Math.floor(Math.random() * 100); 
+//     randomArray.push(randomNumber);
+//   }
+//   return randomArray;
+// }
+// var array = generateRandomArray();
+// console.log(array)
+
+// LEVEL 2
+
+//  14
+
+
+// let arr =  ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"] ;
+// for (let i=0;i<arr.length;i++){
+//   console.log(arr[i])
+// }
+
+// 15
+
+// let arr = ['banana', 'orange', 'mango', 'lemon'] ;
+// for (let i = arr.length-1;i>=0;i--){
+//   console.log(arr[i])
+// }
+
+// 16
+
+
+// 3
+
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB'
+// ]
+// console.log(webTechs.sort())
+
+// const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+// console.log(mernStack.sort())
+
+// 4
+
+// const arr = [
+//   'Albania',
+//   'Bolivia',
+//   'Canada',
+//   'Denmark',
+//   'Ethiopia',
+//   'Finland',
+//   'Germany',
+//   'Hungary',
+//   'Ireland',
+//   'Japan',
+//   'Kenya'
+// ]
+
+// let newArr= arr.filter(e=>e.toLowerCase().includes('land'))
+// console.log(newArr)
+
+
+// 5
+
+// const arr = [
+//   'Albania',
+//   'Bolivia',
+//   'Canada',
+//   'Denmark',
+//   'Ethiopia',
+//   'Finland',
+//   'Germany',
+//   'Hungary',
+//   'Ireland',
+//   'Japan',
+//   'Kenya'
+// ]
+
+// let newArr=arr.filter(country => country.length===5)
+// console.log(newArr)
+
+// const arr = [
+//   'Albania',
+//   'Bolivia',
+//   'Canada',
+//   'Denmark',
+//   'Ethiopia',
+//   'Finland',
+//   'Germany',
+//   'Hungary',
+//   'Ireland',
+//   'Japan',
+//   'Kenya'
+// ]
+
+// let newArr= arr.map(e => e.toUpperCase()).reverse();
+// console.log(newArr)
