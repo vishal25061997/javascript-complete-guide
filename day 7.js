01244d7c6a5ad8aa1ec98d0eff594d69d7773adc
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-05-29 13:22:42 +0530

    day 7

---
+++ day 7.js
@@ -0,0 +1,133 @@
+// DAY 7
+
+// 1
+
+// function fullName(){
+//     let first ='vishal';
+//     let last = 'yadav';
+//     let space = ' '
+//     let fullName = first +space+last;
+//     return fullName
+// }
+// console.log(fullName(fullName))
+
+// 2
+
+// function fullName(p1,p2){
+//   return (p1+' '+ p2)
+// }
+// console.log(fullName('vishal','yadav'))
+
+// 3
+
+// function addNumbers(n1,n2){
+//  let  sum = n1+n2
+//   return sum
+// }
+// console.log(addNumbers(23,20))
+
+// 4
+
+// function area(l,w){
+//   let area = l*w;
+//   return area
+// }
+// console.log(area(5,8))
+
+// 5
+
+// function perimeter(l,w){
+//   let perim = 2*(l+w)
+//   return perim
+// }
+// console.log(perimeter(33,9))
+
+// 6
+
+// let volumeOfRectPrism=(l,w,h)=>{
+//   let volume = l*w*h
+//   return volume
+// }
+// console.log(volumeOfRectPrism(3,4,6))
+
+// 7
+
+// function areaOfCircle(r){
+//   let area = Math.PI*r*r
+//   return area
+// }
+// console.log(areaOfCircle
+// (7))
+
+// 8
+
+// function circumOfCircle(r){
+//   let circum = 2*Math.PI*r
+//   return circum
+// }
+// console.log(circumOfCircle(7))
+
+// 9
+
+// function density(m,v){
+//   let d = m/v
+//   return d
+// }
+// console.log(density(80,9))
+
+// LEVEL 2 
+
+// 1
+
+// function solveLinEquation(a, b, c, x, y) {
+//   return (a * x) + (b * y) + c;
+// }
+// const result = solveLinEquation(2, 3, 4, 5, 6);
+// console.log(result);
+
+
+// 2
+
+// function printArray(arr){
+//   for (let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+//   }
+// }
+// const myArray=[1,4,6,7,5,4,3]
+// printArray(myArray)
+
+// 5
+
+// function swapValues(a,b){
+//    a=a+b
+//    b=a-b
+//    a=a-b
+//   return a+','+b
+// }
+// console.log(swapValues(4,5))
+
+// 6
+
+
+// function printArray(arr){
+//   let newArr = []
+//   for (let i=arr.length-1;i>=0;i--){
+//     newArr.push(arr[i])
+//   }
+//   return newArr
+// }
+// let myArr = [1,2,4,0,7,9]
+// console.log(printArray(myArr))
+
+// 7
+
+// function printArray(arr){
+//   let newArr = []
+//   for (let i=0;i<arr.length;i++){
+//     newArr.push(arr[i].toUpperCase())
+//   }
+//   return newArr
+// }
+// let myArr = ['edkj','skj','sks']
+// console.log(printArray(myArr))
+
