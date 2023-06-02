62e9f96f1def5cafb4c351fe0999bd435fd02cf7
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-06-02 16:21:55 +0530

    day 7.1

---
+++ day7.1.js
@@ -0,0 +1,50 @@
+// 14
+
+
+// function addTwoNum (n1,n2){
+    //     let sum = n1+n2
+    //     return sum
+    
+    // }
+    
+    // console.log(addTwoNum(3,5))
+    
+    
+    // const generateIp=()=>{
+    //   let ips=[];
+    //   for (let i=0;i<4;i++)
+    //     {
+    //       ips.push(Math.floor(Math.random() *100));
+          
+    //     }
+    //   return ips.join(',')
+      
+    // }
+    // console.log(generateIp())
+    
+    
+    // function randomIdGen(){
+    //   let ranDigit='92u29438d2'
+    //   let ranNumber=""
+    //   for(let i=0;i<6;i++){
+    //     ranNumber+= ranDigit[Math.floor(Math.random() *10)]
+    //   }
+    //   return ranNumber
+    // }
+    // console.log(randomIdGen())
+    
+    
+    // function isPrime(num){
+    //   if(num<= 1){
+    //     return false
+    //   }
+    //   for(let i=2;i<= Math.sqrt(num);i++){
+    //     if(num % i ===0){
+    //       return false
+    //     }
+    //   }
+    //   return true
+    // }
+    // console.log(isPrime(6))
+    // console.log(isPrime(7))
+    // console.log(isPrime(3))
