35c579b149bc00330bfed5f0e940bcfdfb01004d
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-06-04 03:23:26 +0530

    set,map

---
+++ day 10.js
@@ -0,0 +1,65 @@
+// const companies = new Set()
+// console.log(companies)
+
+// const languages = [
+//   'English',
+//   'Finnish',
+//   'English',
+//   'French',
+//   'Spanish',
+//   'English',
+//   'French',
+// ]
+// const newSet =new Set(languages)
+// console.log(newSet)
+
+
+// const languages = [
+//   'English',
+//   'Finnish',
+//   'English',
+//   'French',
+//   'Spanish',
+//   'English',
+//   'French',
+// ]
+// const langSet = new Set(languages)
+// console.log(langSet) 
+// console.log(langSet.size) 
+
+// const counts = []
+// const count = {}
+
+// for (const l of langSet) {
+//   const filteredLang = languages.filter((lng) => lng === l)
+//   console.log(filteredLang) 
+//   counts.push({ lang: l, count: filteredLang.length })
+// }
+// console.log(counts)
+
+
+// const countriesMap = new Map()
+// console.log(countriesMap.size) 
+// countriesMap.set('Finland', 'Helsinki')
+// countriesMap.set('Sweden', 'Stockholm')
+// countriesMap.set('Norway', 'Oslo')
+// console.log(countriesMap)
+// console.log(countriesMap.size)
+// console.log(countriesMap.get('Finland'))
+
+
+// const languages = [
+//   'English',
+//   'Finnish',
+//   'English',
+//   'French',
+//   'Spanish',
+//   'English',
+//   'French',
+// ]
+
+// const setOfLanguages = new Set(languages)
+
+// for (const language of setOfLanguages) {
+//   console.log(language)
+// }    
