be5f8317d5e7b124860042e9f61b6b3738524401
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-06-03 13:55:29 +0530

    Objects

---
+++ day 8.js
@@ -0,0 +1,130 @@
+// OBJECTS
+
+// let a='js';
+// let b=22;
+// function letLearnJs(){
+//   let value=false
+//   if (true){
+//     let a='python'
+//     let b=39;
+//     c=44;
+//     value=!value
+//     console.log(value,a,b,c)
+//   }
+  
+// }
+// letLearnJs()
+
+
+
+
+// function letsLearnScope() {
+//   const gravity = 9.812
+//   console.log(gravity)
+// }
+// if (true){
+//   const  gravity = 9.81
+//   console.log(gravity) 
+// }
+// for(let i = 0; i < 3; i++){
+//   console.log(i) 
+// }
+// letsLearnScope()
+
+
+// objects
+
+// const rectangle = {
+//   length: 20,
+//   width: 20
+// }
+// console.log(rectangle) 
+
+// const person = {
+//   firstName: 'Asabeneh',
+//   lastName: 'Yetayeh',
+//   age: 250,
+//   country: 'Finland',
+//   city: 'Helsinki',
+//   skills: [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Node',
+//     'MongoDB',
+//     'Python',
+//     'D3.js'
+//   ],
+//   isMarried: true
+// }
+// console.log(person.firstName)
+
+
+
+// const person = {
+//   firstName: 'Asabeneh',
+//   lastName: 'Yetayeh',
+//   age: 250,
+//   country: 'Finland',
+//   city: 'Helsinki',
+//   skills: [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Node',
+//     'MongoDB',
+//     'Python',
+//     'D3.js'
+//   ],
+//   getFullName: function() {
+//     return `${this.firstName} -${this.lastName}`
+//   },
+//   'phone number': '+3584545454545'
+// }
+
+// console.log(person.firstName)
+// console.log(person.lastName)
+// console.log(person.age)
+// console.log(person.location) 
+
+
+// console.log(person['firstName'])
+// console.log(person['lastName'])
+// console.log(person['age'])
+// console.log(person['age'])
+// console.log(person['location']) 
+// console.log(person['phone number'])
+// console.log(person.getFullName())
+
+
+// person.firstName = 'Ethiopian'
+// person.country = 'Finland'
+// person.title = 'teacher'
+// person.skills.push('Meteor')
+// person.skills.push('SasS')
+// person.isMarried = true
+// console.log(person.firstName)
+
+
+
+// const person = {
+//   firstName: 'Asabeneh',
+//   age: 250,
+//   country: 'Finland',
+//   city:'Helsinki',
+//   skills: ['HTML', 'CSS', 'JS'],
+//   title: 'teacher',
+//   address: {
+//     street: 'Heitamienkatu 16',
+//     pobox: 2002,
+//     city: 'Helsinki'
+//   },
+//   getPersonInfo: function() {
+//     return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
+//   }
+// }
+
+// console.log(Object.keys(person))
+// console.log(Object.keys(person.address))
