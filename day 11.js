3b2debe9063e1da5bea5db357b24e2fc3fa0bf22
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-06-04 14:18:08 +0530

    destructuring-spread

---
+++ day 11.js
@@ -0,0 +1,48 @@
+
+// DESTRUCTURING AND SPREADING
+
+// const names = ['Asabeneh', 'Brook', 'David', 'John']
+  // let [firstPerson, secondPerson, thirdPerson, fourthPerson] = names
+
+  // console.log(firstPerson, secondPerson,thirdPerson, fourthPerson)
+
+// const rect={
+//   length:34,
+//   width :3
+// }
+
+// const calculatePerimeter=(e)=>
+//   2*(e.length+e.width)
+// console.log(calculatePerimeter(rect))
+
+
+// const user = {
+//   name:'Asabeneh',
+//   title:'Programmer',
+//   country:'Finland',
+//   city:'Helsinki'
+// }
+
+// const copiedUser = {...user, title:'instructor'}
+// console.log(copiedUser)
+
+
+// const sumAllNums = (...args) => {
+//   let sum = 0
+//   for (const num of args){
+//     sum += num
+//   }
+//   return sum
+  
+// }
+
+// console.log(sumAllNums(1, 2, 3, 4, 5))
+
+// const constants = [2.72, 3.14, 9.81, 37, 100]
+// let [ e, pi, gravity, humanBodyTemp, waterBoilingTemp]=constants
+// console.log( e, pi, gravity, humanBodyTemp, waterBoilingTemp)
+
+// const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+
+// let [ fin, est, sw, den, nor]=countries
+// console.log( fin, est, sw, den, nor)
