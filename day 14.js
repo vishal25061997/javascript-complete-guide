035a2f0c4472b27a528c95c43ed3ae3b1660b499
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-06-04 15:52:12 +0530

    try and catch

---
+++ day 14.js
@@ -0,0 +1,26 @@
+
+// TRY AND CATCH ERROR
+// try {
+//   let lastName = 'Yetayeh'
+//   let fullName = fistName + ' ' + lastName
+// } catch (err) {
+//   console.log(err)
+// }
+
+// ERROR types
+
+// Refrence error
+
+// let firstName = 'Asabeneh'
+// let fullName = firstName + ' ' + lastName
+// console.log(fullName)
+
+// syntax error
+
+// let square= 2 x 3
+// console.log(square)
+
+// type error
+
+// let a=10;
+// console.log(a.toUpperCase())
